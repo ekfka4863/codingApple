2c0013694240ae8f0128a96a8139f86d806e7869
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-30 00:09:17 +0900

    [Docs] interim commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,59 +1,72 @@
 /* eslint-disable*/ 
 
 import React, { useState } from "react";
-import "../src/App.css";
+import  "../src/App.css";
 
 
 function App() {
-  const [title, setTitle] = useState(["남자 코트 추천", "여자 코트 추천", "강남 직장인 데일리 코디"]);
+  const [title, setTitle] = useState(["서울 맛집", "경기도 맛집", "강원도 맛집",]);
   const [modal, setModal] = useState(false);
-  // step-1
   const [index, setIndex] = useState(0);
+  // const [input, setInput] = useState("");
 
-  // Component
+  return (
+    <div className="App">
+      <div className="black-nav ">
+        <div>개발 Blog</div>
+      </div>
+
+      {/* <div className="list">
+        <h3>제목1</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>제목2</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>제목3</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div> */}
+      
+      {
+        title.map((eachTitle, idx) => {
+          return (<div className="list">
+              <h3 onClick={() => {setIndex(idx++)}}>{ eachTitle }</h3>
+              <p>2월 17일 발행</p>
+              <hr />
+            </div>);
+        })
+      }
+
+      {/* <input onInput={(e) => {setInput(e.target.value)}} type="text" />; */}
+
+      <div className="publish">
+        <input type="text" />
+        <button>저장</button>
+      </div>
+
+
+      <button onClick={()=> {setModal(!modal)}}>모달 열고 닫기 버튼</button>
+      {modal ? <Modal myTitle={title} currIndex={index} /> : null}
+
+    </div>
+  ); 
+
+
+  // Components 
   function Modal (props) {
     return (
       <div className="modal">
-        {/* step-2 */}
-        <h2> { props.myTitle[index] } </h2>
+        <h2>{props.myTitle[props.currIndex]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
     );
   }
-
-
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 Blog</div>
-      </div>
-      
-      {
-        title.map((eachTitle) => {
-
-          return (
-          <div className="list">
-            <h3>{ eachTitle }</h3>
-            <p>2월 17일 발행</p>
-            <hr />
-          </div>
-          )
-
-        })
-      }
-
-      {/* step-3 */}
-      <button onClick={ () => {setIndex(0)}}>버튼 1</button>
-      <button onClick={ () => {setIndex(1)}}>버튼 2</button>
-      <button onClick={ () => {setIndex(2)}}>버튼 3</button>
-
-      <button onClick={() => {setModal(!modal)}}>모달창 열고 닫기 버튼</button>
-      {/* step-4: props 전달 */}
-      {modal ? <Modal myTitle={title} index={index} /> : null}
-
-    </div>
-  );
 }
 
 
