95400ff06dff331f565399c1f9e085ee0c88d682
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-29 18:10:33 +0900

    [Add] modal: when title is clicked display modal window - using ternary operator and useState()

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,14 @@
 /* eslint-disable*/ 
 
-import React, { useLayoutEffect, useState } from "react";
+import React, { useState } from "react";
 import "../src/App.css";
 
 
 function App() {
-  let posts = "강남 고기 맛집";
-  
-  const [title, setTitle] = useState(["남자 코트 추천", "여자 코트 추천", "직장인 데일리 코디"]);
-  // console.log(title);   // (3) ['남자 코트 추천', '여자 코트 추천', '직장인 데일리 코디']
-
+  const [title, setTitle] = useState(["남자 코트 추천", "여자 코트 추천", "ㄱ 직장인 데일리 코디"]);
   const [like, setLike] = useState(0);
+  const [modal, setModal] = useState(false); 
+
   const onClickLike = () => {
     setLike(like + 1);
   };
@@ -22,13 +20,24 @@ function App() {
     // setTitle(["여자 코트 추천", "여자 코트 추천", "직장인 데일리 코디"]);
 
     // 방법 2: 사본 만들기 (단, 얕은 복사 말고 깊은 복사로 - cf. es6 spread 문법)
-    const newArr = title;    // 위와 같이 해도 되지만, title 배열의 사본을 만들어서 사본의 [0] 번째 인덱스에 있는 문자열을 바꿔준다
-    newArr[0] = "여자 코트 추천";
-    console.log(newArr[0]);     // "여자 코트 추천"
+    // const newArr = title;    // 위와 같이 해도 되지만, title 배열의 사본을 만들어서 사본의 [0] 번째 인덱스에 있는 문자열을 바꿔준다
+    // newArr[0] = "여자 코트 추천";
+    // console.log(newArr[0]);     // "여자 코트 추천"
     // 근데, 안 바뀐다 .. 왜? 서로 독립적인 값을 가지는 복사인 deepcopy를 하지 않아서..!! 위에서 한거는 복사가 아니라 "값공유"일 뿐!!
     const newArrDeepCopied = [...title]; 
-    console.log(newArrDeepCopied);
+    // console.log(newArrDeepCopied);
     setTitle(newArrDeepCopied);
+  };
+
+  const onClickSort =() => {
+    const newArr = [...title].sort();   // 딥카피!! 필수!!
+    console.log(newArr);
+    setTitle(newArr);
+  };
+
+  const onClickModal =() => {
+    setModal(!modal);
+    console.log(modal);
   };
 
   return (
@@ -38,6 +47,7 @@ function App() {
       </div>
 
       <button onClick={onClickTitle}>버튼</button>
+      <button onClick={onClickSort}>정렬</button>
 
       <div className="list">
         <h3>{ title[0] } <span onClick={onClickLike}>👍</span> {like} </h3>
@@ -50,14 +60,30 @@ function App() {
         <hr />
       </div>
       <div className="list">
-        <h3>{ title[2] }</h3>
+        <h3 onClick={onClickModal}>{ title[2] }</h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
 
+      {/* <Modal></Modal>
+      <Modal /> */}
+      {modal ? <Modal /> : null}
+      {/* 위에서 null은 텅빈 HTML 이라는 의미 */}
 
     </div> 
   );
+
+  // Component 
+  function Modal() {
+    return (
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    );
+  }
+
 }
 
 export default App;
