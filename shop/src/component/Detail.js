f4ca5f51b9c3b57e74534f31ebd203e569404f1f
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-12-31 15:23:42 +0900

    [Docs] HW: how to use useEffect Hook

--- shop/src/component/Detail.js
+++ shop/src/component/Detail.js
@@ -31,7 +31,7 @@ function Detail(props) {
 
   // useEffect 사용 -> 컴포넌트가 mount 되었을 때, 컴포넌트가 update 될 때 특정 코드를 실행할 수 있다
   useEffect(() => {
-    console.log("hello")
+    // console.log("hello")
   });
   // e.g.  http://localhost:3000/detail/1  -> 에서 보면 console.log("hello") 확인 가능!
 
@@ -39,27 +39,35 @@ function Detail(props) {
   let [alertDisplay, setAlertDisplay] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    let timer = setTimeout(() => {
       // alertDisplay 2초 후에 false로 만들어서 삼항연산자에서 null 반환하기 
       setAlertDisplay(false)
     }, 2000);
+    return (() => { clearTimeout(timer) });  // 나중에 버그 생기지 않게 타이머 setTimeout 없애주기!
   }, []);
-  
+
+  // input에 입력한 데이터를 state에 저장하려고 한다면 ..?
+  const [inputData, setInputData] = useState("");
+  const onInput = (e) => {
+    setInputData(e.target.value);
+    // console.log(inputData);
+  };
+
   return (
     <div className="container">
       <H4 color="red">Detail</H4>
       
-      {(alertDisplay) ? <div className="myAlert"><p>재고가 얼마 남지 않았습니다</p></div> : null}
+      {inputData}
+      <input onInput={onInput} type="text" />
 
-      {/* <div className="myAlert">
-        <p>재고가 얼마 남지 않았습니다</p>
-      </div> */}
-      {/* <div className="myAlert2">
-        <p>재고가 없으면 알림 설정도 가능합니다</p>
-      </div>
-      <div className="myAlert3">
-        <p>믹스인 연습 alert</p>
-      </div> */}
+      {
+        (alertDisplay) 
+        ? (<div className="myAlert">
+            <p>재고가 얼마 남지 않았습니다</p>
+          </div>) 
+        : null
+      }
+
 
       <div className="row">
         <div className="col-md-6">
